8e8ddcf6b71c6eaa9b91de0079eedd9d4a277e52
Donelys Familia <donelysprivate@gmail.com>
2019-10-19 23:00:37 -0400

    adding on submit()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTopicComponent } from './components/add-topic/add-topic.component';
 import { TopicsComponent } from './components/topics/topics.component';
 import { TopicItemComponent } from './components/topic-item/topic-item.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTopicComponent,
     TopicsComponent,
-    TopicItemComponent
+    TopicItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-topic/add-topic.component.ts
+++ src/app/components/add-topic/add-topic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-add-topic',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTopicComponent implements OnInit {
 
+  keyword: string;
+  name:string;
+  contributor:string;
+  contributor_email:string;
+  description: string;
+  
+  isCollapsed:boolean = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleCollapse(){
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  onSubmit(){
+    const topic = {
+      keyword: this.keyword,
+      name: this.name,
+      contributor: this.contributor,
+      contributor_email: this.contributor_email,
+      description:this.description
+
+    }
   }
 
 }
